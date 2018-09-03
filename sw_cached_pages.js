c99b3551fbad4fd68bd98c6108c8d5aca7eae433
papanugget <evilgenius04@gmail.com>
2018-09-03 17:31:35 -0400

    add service worker js file

---
+++ sw_cached_pages.js
@@ -0,0 +1,54 @@
+// cache vars
+const cacheName = 'v1';
+const cacheAssets = [
+    './index.html',
+    './restaurant.html',
+    './css/style.css',
+    './data/restaurants.json',
+    './img/*.jpg',
+    './js/*.js'
+];
+
+// call install event
+self.addEventListener('install', (e) => {
+    console.log('Service worker: Installed');
+});
+
+// call activate event
+self.addEventListener('activate', (e) => {
+    console.log('Service worker: Activated');
+    // remove unwanted caches
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if(cache !== cacheName){
+                        console.log('Service worker: Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// call fetch event
+self.addEventListener('fetch', e => {
+    console.log('Service worker: fetching');
+    e.respondWith(
+        fetch(e.request)
+        .then(res => {
+            // make a clone of response
+            const resClone = res.clone();
+            // open cache
+            caches
+                .open(cacheName)
+                .then(cacheObj => {
+                    // add response to cache
+                    cacheObj.put(e.request, resClone);
+            });
+            return res;
+            // if connection drops catch will run
+        }).catch(err => caches.match(e.request).then(res => res))
+    );
+});

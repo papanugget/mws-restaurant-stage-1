1eae91437e260d63a1d5781104e6d1bbfd9fe7d5
papanugget <evilgenius04@gmail.com>
2018-09-04 11:51:04 -0400

    added offline pages to cache

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -2,7 +2,16 @@
 const cacheName = 'v2';
 const cacheAssets = [
     'index.html',
-    'restaurant.html',
+    'restaurant.html?id=1',
+    'restaurant.html?id=10',
+    'restaurant.html?id=2',
+    'restaurant.html?id=3',
+    'restaurant.html?id=4',
+    'restaurant.html?id=5',
+    'restaurant.html?id=6',
+    'restaurant.html?id=7',
+    'restaurant.html?id=8',
+    'restaurant.html?id=9',
     'css/styles.css',
     'data/restaurants.json',
     'img/1.jpg',
